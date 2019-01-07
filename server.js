7c345e0ec1f425c52ff3c012af530d2e265d2e0f
Joao Maques <joao.martinho.marques95@gmail.com>
2019-01-07 15:44:31 +0000

    heroku

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express=require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
+
+const port = process.env.PORT || 3000;
 var app = express();
 
 
@@ -31,4 +33,4 @@ app.get('/bad', (req,res)=>{
     res.send('Bad request');
 })
 
- app.listen(3000);
+ app.listen(port);
